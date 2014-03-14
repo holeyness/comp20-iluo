ff45ead35815015d7f31cd46397a46d77c76f3f0
holeyness <ian@ianluo.com>
2014-03-14 03:58:20 -0400

    Minor Bug fix

--- transit/map.js
+++ transit/map.js
@@ -4,10 +4,9 @@ function init(){
 		center:new google.maps.LatLng(42.404796, -71.119884),
 		zoom:5,
 		mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("canvas"),mapcanvas);
-}
-	
+		};
+ 
+	var map=new google.maps.Map(document.getElementById("canvas"),mapcanvas);
 }
 
 function stations(){
