cb71e1ac51b807dcd2426722eaa681a4220795fd
holeyness <ian@ianluo.com>
2014-03-14 11:45:53 -0400

    Bug fix

--- transit/map.js
+++ transit/map.js
@@ -82,7 +82,7 @@ function calculate(lat1,long1,lat2,long2){
 	var dLat = toRad((lat2-lat1));
 	var dLon = toRad((lon2-lon1));
 	var lat1 = toRad(lat1);
-	var lat2 = toRad(lat2.);
+	var lat2 = toRad(lat2);
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
