f4d4a161eca25b589089a1dbd31db6ae412dacf4
holeyness <ian@ianluo.com>
2014-03-14 11:42:38 -0400

    Added to Rad Function

--- transit/map.js
+++ transit/map.js
@@ -204,3 +204,14 @@ var stations =
 {"Color":"Red","Station":"Shawmut","Lat":42.29312583,"Long":-71.06573796},
 {"Color":"Red","Station":"South Station","Lat":42.352271,"Long":-71.055242},
 {"Color":"Red","Station":"Wollaston","Lat":42.2665139,"Long":-71.0203369}];
+
+
+//helper to rad function
+/** Converts numeric degrees to radians */
+function toRad(){
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  }
+}
+}
