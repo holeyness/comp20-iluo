3971ef4f62d34c5f3e88d1586231e5a5d4d009d7
holeyness <ian@ianluo.com>
2014-03-25 00:38:45 -0400

    Bug fix for testing

--- transit/map.js
+++ transit/map.js
@@ -203,10 +203,11 @@ function drawStations(){
 
 		for (i = 0; i < bluestations.length; i++){
 			var image = 'marker.png';
-			trains(bluestations[i].Station);	//order the array
-			var inboundtime = traintimes(inbound);
-			var outboundtime = traintimes(outbound);
-			var stationcontent = '<div id = "title">'+'<h3>Station: '+ bluestations[i].Station +'</h3>'+'</div>'+'<div id="inbound">'+ inboundtime + '</div>' + '<div id = "outbound">'+ outboundtime + '</div>';
+			//trains(bluestations[i].Station);	//order the array
+			//var inboundtime = traintimes(inbound);
+			//var outboundtime = traintimes(outbound);
+			//	var stationcontent = '<div id = "title">'+'<h3>Station: '+ bluestations[i].Station +'</h3>'+'</div>'+'<div id="inbound">'+ inboundtime + '</div>' + '<div id = "outbound">'+ outboundtime + '</div>';
+			var stationcontent = bluestations[i].Station;
 
 			var myLatLng = new google.maps.LatLng(bluestations[i].Lat, bluestations[i].Long);
 			var stationMarker = new google.maps.Marker({
